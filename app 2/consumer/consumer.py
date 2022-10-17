bc9875d97a6183fff7acc700e4c5b413e11216cc
Gaureesh Anvekar <gaureesh@Gaureeshs-MacBook-Pro.local>
2022-10-16 23:34:58 -0700

    changes

--- app 2/consumer/consumer.py
+++ app 2/consumer/consumer.py
@@ -22,4 +22,4 @@ def index():
 if __name__ == "__main__":
     # port = int(os.environ.get('PORT', 3001))
     # app.run('127.0.0.1', port = port,debug=True)
-    app.run(host='0.0.0.0', port='80')
+    app.run(host='0.0.0.0', port=os.environ.get("CONSUMER_PORT"))
